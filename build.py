c56ad6f30964f4e1578acb5040a5652427488309
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-03-26 12:08:21 +0530

    Added black.yml workflow and run black

--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 from ftd import fpm_build
 import asyncio
 
+
 async def build(file=None, base_url=None, ignore_failed=None):
     await fpm_build(file, base_url, ignore_failed)
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(build())
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-with open('README.md') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
-    author='Amit Upadhyay',
-    author_email='amitu@fifthtry.com',
-    description='A PyPI package to use FTD',
+    author="Amit Upadhyay",
+    author_email="amitu@fifthtry.com",
+    description="A PyPI package to use FTD",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    name='ftd',
-    url='https://github.com/FifthTry/ftd-py',
+    long_description_content_type="text/markdown",
+    name="ftd",
+    url="https://github.com/FifthTry/ftd-py",
     project_urls={
         "Bug Tracker": "https://github.com/FifthTry/ftd-py/issues",
     },
