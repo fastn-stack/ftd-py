9069647c8ed213c42b5106985d9f48a3a8e4a1d3
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-04-06 20:28:45 +0530

    added ftd.render function

---
+++ render.py
@@ -0,0 +1,35 @@
+import json
+
+import ftd
+import asyncio
+
+# class Document:
+#     async def __init__(self, f, **data):
+#         self.f = f
+#         self.data = data
+#
+#     async def render(self, **data):
+#         data += self.data
+#         data = json.dumps(data)
+#         await ftd.render(self.f, data)
+#
+#
+# async def parse(f, **data):
+#     return Document(f, **data)
+#
+#
+# async def hello():
+#     d = await parse("foo.ftd", message="hello world", n=10)
+#     await d.render()
+
+async def build():
+    data = {"message": "hello world", "n": 10}
+    data = json.dumps(data)
+    await ftd.render("foo.ftd", data)
+
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(build())
+loop.close()
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,36 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pyfunction]
+fn render<'a>(py: pyo3::Python<'a>, file: String, data: String) -> PyResult<&'a PyAny> {
+    // dbg!(&data, data.get_type(), data.get_type_ptr());
+    // for k in data.iter()? {
+    //     let g = k?;
+    //     dbg!(&g, data.get_item(g));
+    // }
+    pyo3_asyncio::tokio::future_into_py(py, async move {
+        let config = {
+            let mut config = match fpm::Config::read().await {
+                Ok(c) => c,
+                _ => {
+                    return Ok(Python::with_gil(|py| py.None()));
+                }
+            };
+            config.extra_data = match serde_json::from_str(data.as_str()) {
+                Ok(c) => c,
+                _ => {
+                    return Ok(Python::with_gil(|py| py.None()));
+                }
+            };
+            config
+        };
+        fpm::build(&config, Some(file.as_str()), "/", false)
+            .await
+            .ok();
+        Ok(Python::with_gil(|py| py.None()))
+    })
+}
+
+#[pyfunction]
 fn fpm_build(
     py: pyo3::Python,
     file: Option<String>,
@@ -32,10 +62,23 @@ fn fpm_build(
     })
 }
 
+/*#[pyfunction]
+fn parse(
+    filename: &str,
+    data: std::collections::BTreeMap<String, String>,
+) -> PyResult<std::collections::BTreeMap<String, String>> {
+    Ok(File {
+        filename: filename.to_string(),
+        data,
+    })
+}*/
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn ftd(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(fpm_build, m)?)?;
+    // m.add_function(wrap_pyfunction!(parse, m)?)?;
+    m.add_function(wrap_pyfunction!(render, m)?)?;
     Ok(())
 }
