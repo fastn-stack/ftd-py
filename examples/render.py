6407364b898d5018dce37774d71df2e549324c16
Amit Upadhyay <upadhyay@gmail.com>
2022-04-15 21:03:35 +0530

    why are these not working!

--- examples/render.py
+++ examples/render.py
@@ -1,13 +1,3 @@
 import ftd
-import asyncio
 
-
-async def render():
-    d = ftd.parse("foo", message="FTD renderer says hello world", n=10)
-    return await d.render(n=12)
-
-
-loop = asyncio.get_event_loop()
-res = loop.run_until_complete(render())
-print(res)
-loop.close()
+print(ftd.render_sync("../sample", "/", message="FTD renderer says hello world", n=10))
--- ftd_sys/render.py
+++ ftd_sys/render.py
@@ -3,30 +3,11 @@ import json
 import ftd_sys
 import asyncio
 
-# class Document:
-#     async def __init__(self, f, **data):
-#         self.f = f
-#         self.data = data
-#
-#     async def render(self, **data):
-#         data += self.data
-#         data = json.dumps(data)
-#         await ftd.render(self.f, data)
-#
-#
-# async def parse(f, **data):
-#     return Document(f, **data)
-#
-#
-# async def hello():
-#     d = await parse("foo.ftd", message="hello world", n=10)
-#     await d.render()
-
 
 async def render():
     data = {"asd": "hello world!!!", "n": 10, "kv2": {"key": "asd2", "value": "something2"}}
     data = json.dumps(data)
-    return await ftd_sys.render("sample", "/", data)
+    return await ftd_sys.render("../sample", "/", data)
 
 
 loop = asyncio.get_event_loop()
