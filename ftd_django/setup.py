ea79544f95b1129b6f79fb876d07884e1f12439c
Arpita-Jaiswal <ayushipujaa@gmail.com>
2022-04-28 11:58:06 +0530

    Bump the `ftd_django` to 0.1.2

--- ftd_django/setup.py
+++ ftd_django/setup.py
@@ -15,6 +15,6 @@ setup(
         "Bug Tracker": "https://github.com/FifthTry/ftd-py/issues",
     },
     python_requires=">=3.6",
-    version="0.1.1",
+    version="0.1.2",
     install_requires=["ftd>=0.1.2"],
 )
