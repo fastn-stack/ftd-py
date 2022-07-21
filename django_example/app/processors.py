4ebb01a753bdc6c5d9b3c7190b651b915320ec3f
Heulitig <rithik@fifthtry.com>
2022-07-21 13:55:48 +0530

    added input_id and updated UPDATE api

--- django_example/app/processors.py
+++ django_example/app/processors.py
@@ -1,30 +1,6 @@
 import ftd
 import ftd_django
 
-# Used by to-do table as list of tasks => [name, status] tuples
-data = [
-    {
-        "task_name": "Task Name",
-        "status": "Status",
-        "id": "1"
-    },
-    {
-        "task_name": "ftd application processor for toc data",
-        "status": "Done",
-        "id": "2"
-    },
-    {
-        "task_name": "call an api to update todo list",
-        "status": "In Progress",
-        "id": "3"
-    },
-    {
-        "task_name": "documentation for all",
-        "status": "Pending",
-        "id": "4"
-    }
-]
-
 @ftd_django.processor("hello_world")
 def hello_world(doc_id, section, interpreter):
     return ftd.string_to_value("This is coming from processor")
@@ -32,4 +8,27 @@ def hello_world(doc_id, section, interpreter):
 
 @ftd_django.processor
 def todo_data(doc_id, section, interpreter):
+    # Used by to-do table as list of tasks => [name, status, tid] per task
+    data = [
+        {
+            "task_name": "Task Name",
+            "status": "Status",
+            "tid": 1
+        },
+        {
+            "task_name": "ftd application processor for toc data",
+            "status": "Done",
+            "tid": 2
+        },
+        {
+            "task_name": "call an api to update todo list",
+            "status": "In Progress",
+            "tid": 3
+        },
+        {
+            "task_name": "documentation for all",
+            "status": "Pending",
+            "tid": 4
+        }
+    ]
     return ftd.object_to_value(data, section, interpreter)
--- django_example/app/views.py
+++ django_example/app/views.py
@@ -3,6 +3,38 @@ import django.http
 from django.views.generic import FormView, TemplateView
 from django.views.decorators.csrf import csrf_exempt
 
+# Copy of initialized todo list (Testing)
+data = [
+    {
+        "task_name": "Task Name",
+        "status": "Status",
+        "tid": 1
+    },
+    {
+        "task_name": "ftd application processor for toc data",
+        "status": "Done",
+        "tid": 2
+    },
+    {
+        "task_name": "call an api to update todo list",
+        "status": "In Progress",
+        "tid": 3
+    },
+    {
+        "task_name": "documentation for all",
+        "status": "Pending",
+        "tid": 4
+    }
+]
+
+def update_todo_item(target_tid, status):
+    for task in data:
+        curr_id = task['tid']
+        if curr_id == target_tid:
+            task['status'] = status
+            return True
+
+    return False
 
 class IndexView(TemplateView):
     template_name = "/foo/"
@@ -51,7 +83,30 @@ def post_data(req: django.http.HttpRequest):
 
 @csrf_exempt
 def update_todo(req: django.http.HttpRequest):
+    from django.http import JsonResponse
+
     body = json.loads(req.body)
+    target_tid = body['tid']
+    status = body['status']
     print(f"body received = {body}")
+    print(f"tid received = {target_tid}")
+    print(f"status received = {status}")
+
+    update_status = update_todo_item(target_tid,status)
+    s = "success" if update_status else "failure"
+    print(f"update status = {s}")
+    print(f"updated task list = {data}")
+
+    json_data = json.dumps(data)
+    print(json_data)
+
+    return JsonResponse(
+        {
+            "data": {
+                "foo#items": json_data,
+            }
+        },
+        status=200,
+    )
 
     # print(json.loads(req.body))
