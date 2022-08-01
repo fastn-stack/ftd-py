4166a344bb673d31cc5ae94d4482ce0b6537f6f0
Rithik Seth <106665190+Heulitig@users.noreply.github.com>
2022-08-01 12:25:53 +0530

    django examples, examples, sample folder removed (#15)

--- examples/ftd_render.py
+++
@@ -1,5 +0,0 @@
-import ftd
-
-print(
-    ftd.render_sync("../sample", "foo/", message="FTD renderer says hello world", n=10)
-)
--- examples/ftd_sys_build.py
+++
@@ -1,9 +0,0 @@
-from ftd_sys import fpm_build
-import asyncio
-
-
-async def build(root=None, file=None, base_url=None, ignore_failed=None):
-    await fpm_build(root, file, base_url, ignore_failed)
-
-
-asyncio.run(build(root="../sample"))
